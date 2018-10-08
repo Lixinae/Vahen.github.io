9d81530262dbe6c8b7914c12aad237d0eacf0aa0
Vahen <naeos@laposte.net>
2018-10-09 00:15:03 +0200

    Ajout d'une bannière de test, mis a jour du css pour avoir un menu deroulant basique

--- scripts/js/menu.js
+++ scripts/js/menu.js
@@ -1,8 +1,6 @@
 
 $(document).ready(function() {
-	alert("hello");
 	showMenu();
-	alert("Finished");
 });
 
 /*
@@ -10,12 +8,19 @@ Affiche le menu
 */
 function showMenu(){
 	
-$('.menu').append('</br>');	
-$('.menu').append('<a href="https://vahen.github.io/Projets.html"> Projets </a>');
-$('.menu').append('</br>');
-$('.menu').append('<a href="https://vahen.github.io/Contact.html"> Contact </a>');
-$('.menu').append('</br>');
-$('.menu').append('<a href="https://vahen.github.io/WhoAmI.html"> Qui suis je ? </a>');	
-$('.menu').append('</br>');
+
+$('#menu').append('<div class="dropdown"> <a class="menuElement" href="Projets.html"> Projets </a></div>')
+$('.dropdown').append('<div class="dropdown-content">');
+$('.dropdown-content').append('<a href="./projets/2048.html"> 2048 </a></br>');
+$('.dropdown-content').append('<a href="./projets/MrWheelAdventure.html"> Mr Wheel Adventure </a></br>');
+$('.dropdown-content').append('<a href="./projets/Pacman3d.html"> Pacman 3D </a></br>');
+$('.dropdown-content').append('<a href="./projets/PlatformGame.html"> PlatformGame </a></br>');
+
+ $('.dropdown').append('</div>');
+
+$('#menu').append('<a class="menuElement" href="Contact.html"> Contact </a>');
+
+$('#menu').append('<a class="menuElement" href="WhoAmI.html"> Qui suis je ? </a>');	
+
 	
 }
