73b649ff02799f1afefa8a790c60e681aea240bc
Vahen <naeos@laposte.net>
2018-10-09 01:02:19 +0200

    Ajout du header sur les pages

--- scripts/js/menu.js
+++ scripts/js/menu.js
@@ -8,15 +8,16 @@ Affiche le menu
 */
 function showMenu(){
 	
-$('#menu').append('<a class="menuElement" href="index.html"> Accueil </a>');
-$('#menu').append('<div class="dropdown"> <a class="menuElement" href="Projets.html"> Projets </a></div>')
-$('.dropdown').append('<div class="dropdown-content">');
-$('.dropdown-content').append('<a href="./projets/2048.html"> 2048 </a></br>');
-$('.dropdown-content').append('<a href="./projets/MrWheelAdventure.html"> Mr Wheel Adventure </a></br>');
-$('.dropdown-content').append('<a href="./projets/Pacman3d.html"> Pacman 3D </a></br>');
-$('.dropdown-content').append('<a href="./projets/PlatformGame.html"> PlatformGame </a></br>');
+$('#menu').append('<a class="menuElement" href="/index.html"> Accueil </a>');
+$('#menu').append('<div class="dropdown"> <a class="menuElement" href="Projets.html"> Projets </a></div>');
 
- $('.dropdown').append('</div>');
+$('.dropdown').append('<div class="dropdown-content">');
+$('.dropdown-content').append('<a href="/projets/2048.html"> 2048 </a></br>');
+$('.dropdown-content').append('<a href="/projets/MrWheelAdventure.html"> Mr Wheel Adventure </a></br>');
+$('.dropdown-content').append('<a href="/projets/Pacman3d.html"> Pacman 3D </a></br>');
+$('.dropdown-content').append('<a href="/projets/PlatformGame.html"> PlatformGame </a></br>');
+
+$('.dropdown').append('</div>');
 
 $('#menu').append('<a class="menuElement" href="Contact.html"> Contact </a>');
 
