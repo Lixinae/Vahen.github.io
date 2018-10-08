f928f49ec514aff897c1ef2df0446151d28a2160
Vahen <naeos@laposte.net>
2018-10-08 14:30:05 +0200

    Ajout de scripts

---
+++ scripts/js/menu.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+	
+	showMenu();
+	
+}
+
+/*
+Affiche le menu
+*/
+function showMenu(){
+	
+$('.menu').append('</br><a href="https://vahen.github.io/Projets.html"> Projets </a></br>');
+$('.menu').append('<a href="https://vahen.github.io/Contact.html"> Contact </a></br>');
+$('.menu').append('<a href="https://vahen.github.io/WhoAmI.html"> Qui suis je ? </a></br>');	
+	
+}
