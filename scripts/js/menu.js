ff4c6ee1692c5d7a6addbedb526c6d5f765c3ffd
Vahen <naeos@laposte.net>
2018-10-09 01:15:18 +0200

    Correctifs

--- scripts/js/menu.js
+++ scripts/js/menu.js
@@ -9,19 +9,19 @@ Affiche le menu
 function showMenu(){
 	
 $('#menu').append('<a class="menuElement" href="/index.html"> Accueil </a>');
-$('#menu').append('<div class="dropdown"> <a class="menuElement" href="/Projets.html"> Projets </a></div>');
+$('#menu').append('<div class="dropdown"> <a class="menuElement" href="/pages/Projets.html"> Projets </a></div>');
 
 $('.dropdown').append('<div class="dropdown-content">');
-$('.dropdown-content').append('<a href="/projets/2048.html"> 2048 </a></br>');
-$('.dropdown-content').append('<a href="/projets/MrWheelAdventure.html"> Mr Wheel Adventure </a></br>');
-$('.dropdown-content').append('<a href="/projets/Pacman3d.html"> Pacman 3D </a></br>');
-$('.dropdown-content').append('<a href="/projets/PlatformGame.html"> PlatformGame </a></br>');
+$('.dropdown-content').append('<a href="/pages/projets/2048.html"> 2048 </a></br>');
+$('.dropdown-content').append('<a href="/pages/projets/MrWheelAdventure.html"> Mr Wheel Adventure </a></br>');
+$('.dropdown-content').append('<a href="/pages/projets/Pacman3d.html"> Pacman 3D </a></br>');
+$('.dropdown-content').append('<a href="/pages/projets/PlatformGame.html"> PlatformGame </a></br>');
 
 $('.dropdown').append('</div>');
 
-$('#menu').append('<a class="menuElement" href="/Contact.html"> Contact </a>');
+$('#menu').append('<a class="menuElement" href="/pages/Contact.html"> Contact </a>');
 
-$('#menu').append('<a class="menuElement" href="/WhoAmI.html"> Qui suis je ? </a>');	
+$('#menu').append('<a class="menuElement" href="/pages/WhoAmI.html"> Qui suis je ? </a>');	
 
 	
 }
