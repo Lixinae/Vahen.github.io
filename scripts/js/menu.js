ea21570898ea20d7e70c9967a2c8b1972b10640a
Vahen <naeos@laposte.net>
2018-10-08 14:34:43 +0200

    Correctifs

--- scripts/js/menu.js
+++ scripts/js/menu.js
@@ -10,8 +10,8 @@ Affiche le menu
 */
 function showMenu(){
 	
-$('.menu').append('</br><a href="https://vahen.github.io/Projets.html"> Projets </a></br>');
-$('.menu').append('<a href="https://vahen.github.io/Contact.html"> Contact </a></br>');
-$('.menu').append('<a href="https://vahen.github.io/WhoAmI.html"> Qui suis je ? </a></br>');	
+$('#menu').append('</br><a href="https://vahen.github.io/Projets.html"> Projets </a></br>');
+$('#menu').append('<a href="https://vahen.github.io/Contact.html"> Contact </a></br>');
+$('#menu').append('<a href="https://vahen.github.io/WhoAmI.html"> Qui suis je ? </a></br>');	
 	
 }
