6cfa059df60098786321ce621a0610fb4bee6a6e
Vahen <naeos@laposte.net>
2018-10-09 03:04:22 +0200

    Correctifs

--- scripts/js/projets.js
+++ scripts/js/projets.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$.getScript('menu.js', function(){
+	$.getScript('menu.js', function(e){
 		createClickableLoadPage('#Menu2048',"/pages/projets/2048.html","2048");
 		createClickableLoadPage('#MenuMrWheel',"/pages/projets/MrWheelAdventure.html","MrWheelAdventure");
 		createClickableLoadPage('#MenuPacman',"/pages/projets/Pacman3d.html","Pacman3D");
