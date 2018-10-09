c785e23583fdc881e847c915fc391722a5e08c1f
Vahen <naeos@laposte.net>
2018-10-09 03:17:13 +0200

    Correctifs

--- scripts/js/projets.js
+++ scripts/js/projets.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
-	$.getScript('menu.js', function(e){
-		createClickableLoadPage('#Menu2048',"/pages/projets/2048.html","2048");
-		createClickableLoadPage('#MenuMrWheel',"/pages/projets/MrWheelAdventure.html","MrWheelAdventure");
-		createClickableLoadPage('#MenuPacman',"/pages/projets/Pacman3d.html","Pacman3D");
-		createClickableLoadPage('#MenuPlatform',"/pages/projets/PlatformGame.html","PlatformGame");
-	});	
+	createClickableLoadPage('#Menu2048',"/pages/projets/2048.html","2048");
+	createClickableLoadPage('#MenuMrWheel',"/pages/projets/MrWheelAdventure.html","MrWheelAdventure");
+	createClickableLoadPage('#MenuPacman',"/pages/projets/Pacman3d.html","Pacman3D");
+	createClickableLoadPage('#MenuPlatform',"/pages/projets/PlatformGame.html","PlatformGame");
 });
+
+/*
+Copie de la fonction dans menu js -> doit etre changé plus tard
+*/
+function createClickableLoadPage(id,url,title){
+	$(id).on('click', function(e){
+        e.preventDefault();
+        $("#content").load(url);
+		// Pour modifier l'url sans recharger
+		/*if (history.pushState) {
+			window.history.pushState("", title, url);
+		} else {
+			document.location.href = url;
+		}*/
+    });
+
+}
